7ddf9969acd098b5b8122c8793eba9ac2c718af5
Alexandr Tulisov <shura74084@gmail.com>
2024-07-13 10:48:14 +0500

    Добавлен стор для хранения результатов поиска пользователей

---
+++ src/stores/searchUser.store.js
@@ -0,0 +1,36 @@
+import { defineStore } from 'pinia'
+import { Client, Users } from 'node-appwrite'
+
+const client = new Client()
+
+client
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID)
+  .setKey(import.meta.env.VITE_APPWRITE_API_KEY)
+
+const users = new Users(client)
+
+export const useSearchUserStore = defineStore('searchUser', {
+  state: () => ({
+    users: [],
+    searchUserID: ''
+  }),
+  actions: {
+    async getUsersID(userID = '') {
+      try {
+        if (userID) {
+          const response = await users.get(userID)
+          this.users = [response]
+        } else {
+          this.users = []
+        }
+      } catch (error) {
+        console.log('Не удалось получить список пользователей!', error)
+        this.users = []
+      }
+    },
+    setSearchUserID(userId) {
+      this.searchUserID = userId
+    }
+  }
+})
